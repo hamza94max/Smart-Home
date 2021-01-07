690bd7bb04f0e6163956eb41a9a96f3532dc2c5c
hamzagamal <54688005+hamza94max@users.noreply.github.com>
2021-01-08 00:57:58 +0200

    first

---
+++ smarthome/app/src/main/java/com/example/hamza/smarthome/Fragments/HomeFragment.java
@@ -0,0 +1,46 @@
+package com.example.hamza.smarthome.Fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.hamza.smarthome.R;
+
+
+public class HomeFragment extends Fragment {
+
+
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public HomeFragment() {
+
+    }
+
+
+    public static HomeFragment newInstance() {
+        HomeFragment fragment = new HomeFragment();
+
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+}
---
+++ smarthome/app/src/main/java/com/example/hamza/smarthome/Fragments/MoveFragment.java
@@ -0,0 +1,40 @@
+package com.example.hamza.smarthome.Fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.hamza.smarthome.R;
+
+
+public class MoveFragment extends Fragment {
+
+
+    public MoveFragment() {
+        // Required empty public constructor
+    }
+
+
+
+    public static MoveFragment newInstance() {
+        MoveFragment fragment = new MoveFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_move, container, false);
+    }
+}
