e5ffa924e5a0f079fe336283c1277b2740e83f94
hamzagamal <54688005+hamza94max@users.noreply.github.com>
2021-01-24 00:33:36 +0200

    update ui

---
+++ smarthome/app/src/main/java/com/example/hamza/smarthome/Activities/Test.java
@@ -0,0 +1,23 @@
+package com.example.hamza.smarthome.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.os.Bundle;
+
+import com.example.hamza.smarthome.R;
+import com.warkiz.widget.IndicatorSeekBar;
+import com.warkiz.widget.IndicatorType;
+import com.warkiz.widget.TickMarkType;
+
+public class Test extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+
+    }
+}
