5ff3f8d4426161c403024db2ac0f238baaf0dfa5
hamza94max <hamzagamal854@gmail.com>
2021-07-06 21:14:46 +0200

    doors

--- smarthome/app/src/main/java/com/example/hamza/smarthome/Activities/MainActivity.java
+++ smarthome/app/src/main/java/com/example/hamza/smarthome/Activities/MainActivity.java
@@ -2,11 +2,14 @@ package com.example.hamza.smarthome.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
+import androidx.databinding.DataBindingUtil;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.widget.CompoundButton;
+import android.widget.SeekBar;
 import android.widget.Switch;
 import com.example.hamza.smarthome.R;
+import com.example.hamza.smarthome.databinding.ActivityMainBinding;
 import com.xw.repo.BubbleSeekBar;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,20 +20,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        @SuppressLint("UseSwitchCompatOrMaterialCode") Switch led1 = findViewById(R.id.led1);
-        @SuppressLint("UseSwitchCompatOrMaterialCode") Switch led2 = findViewById(R.id.led2);
-        @SuppressLint("UseSwitchCompatOrMaterialCode") Switch led3 = findViewById(R.id.led3);
-        @SuppressLint("UseSwitchCompatOrMaterialCode") Switch led4 = findViewById(R.id.led4);
-        @SuppressLint("UseSwitchCompatOrMaterialCode") Switch led5 = findViewById(R.id.led5);
-        @SuppressLint("UseSwitchCompatOrMaterialCode") Switch led6 = findViewById(R.id.led1_out);
-        @SuppressLint("UseSwitchCompatOrMaterialCode") Switch led7 = findViewById(R.id.led2_out);
+        BubbleSeekBar fan1 = findViewById(R.id.Fan1);
+        BubbleSeekBar fan2 = findViewById(R.id.Fan2);
+        BubbleSeekBar fan3 = findViewById(R.id.Fan3);
+        BubbleSeekBar heater1 = findViewById(R.id.Heater1);
+        BubbleSeekBar heater2 = findViewById(R.id.Heater2);
+        BubbleSeekBar heater3 = findViewById(R.id.Heater3);
 
-        BubbleSeekBar fan1 = findViewById(R.id.seekbar1);
-        BubbleSeekBar fan2 = findViewById(R.id.seekbar2);
-        BubbleSeekBar fan3 = findViewById(R.id.seekbar3);
-        BubbleSeekBar heater1 = findViewById(R.id.seekbar4);
-        BubbleSeekBar heater2 = findViewById(R.id.seekbar5);
-        BubbleSeekBar heater3 = findViewById(R.id.seekbar6);
+
+
+
+
+
+
 
         SeekbarUi(fan1);
         SeekbarUi(fan2);
@@ -44,22 +46,14 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-        Switch auto =findViewById(R.id.auto);
+        @SuppressLint("UseSwitchCompatOrMaterialCode") Switch auto = findViewById(R.id.autoSwitch);
         auto.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
 
             }
         });
-
-
-
-
-
     }
-
-
-
 
     private void SeekbarUi (BubbleSeekBar BbubbleSeekBar){
 
@@ -67,24 +61,22 @@ public class MainActivity extends AppCompatActivity {
                 .min(0)
                 .max(5)
                 .sectionCount(5)
-                .trackColor(ContextCompat.getColor(getApplicationContext(), R.color.back))
-                .secondTrackColor(ContextCompat.getColor(getApplicationContext(), R.color.bluee))
-                .thumbColor(ContextCompat.getColor(getApplicationContext(), R.color.bluee))
+                .trackColor(ContextCompat.getColor(getApplicationContext(), R.color.bluee))
+                .secondTrackColor(ContextCompat.getColor(getApplicationContext(), R.color.textcolor))
+                .thumbColor(ContextCompat.getColor(getApplicationContext(), R.color.textcolor))
                 .showSectionText()
-                .sectionTextColor(ContextCompat.getColor(getApplicationContext(), R.color.dab))
+                .sectionTextColor(ContextCompat.getColor(getApplicationContext(), R.color.textcolor))
                 .sectionTextSize(18)
                 .showThumbText()
                 .thumbTextColor(ContextCompat.getColor(getApplicationContext(), R.color.blackcolor))
                 .thumbTextSize(18)
-                .bubbleColor(ContextCompat.getColor(getApplicationContext(), R.color.colorAccent))
+                .bubbleColor(ContextCompat.getColor(getApplicationContext(), R.color.bluee))
                 .bubbleTextSize(18)
                 .showSectionMark()
                 .seekBySection()
                 .autoAdjustSectionMark()
                 .sectionTextPosition(BubbleSeekBar.TextPosition.BELOW_SECTION_MARK)
                 .build();
-
-
     }
 
 }
